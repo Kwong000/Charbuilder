926dc6e5a5e98fb0b023180182da0420191b7172
Keith <kwong25@scienceleadership.org>
2024-03-05 14:27:42 -0500

    Push

--- src/main/java/com/example/demo1/TimelinePage.java
+++ src/main/java/com/example/demo1/TimelinePage.java
@@ -1,4 +1,40 @@
 package com.example.demo1;
 
+import java.io.File;
+
 public class TimelinePage {
+
+    String title;
+    String textBody;
+    File repIMG;
+    public TimelinePage(String title, String textBody, File repIMG) {
+        this.title = title;
+        this.textBody = textBody;
+        this.repIMG = repIMG;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTextBody() {
+        return textBody;
+    }
+
+    public void setTextBody(String textBody) {
+        this.textBody = textBody;
+    }
+
+    public File getRepIMG() {
+        return repIMG;
+    }
+
+    public void setRepIMG(File repIMG) {
+        this.repIMG = repIMG;
+    }
+
+
 }
