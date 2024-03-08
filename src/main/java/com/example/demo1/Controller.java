909dd0ad7b10b40088958ebe75aae893ff9b1440
Keith <kwong25@scienceleadership.org>
2024-03-08 09:41:40 -0500

    Push

--- src/main/java/com/example/demo1/Controller.java
+++ src/main/java/com/example/demo1/Controller.java
@@ -1,10 +1,20 @@
 package com.example.demo1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class Controller {
 
+
+    public TextArea bodyBox;
+    public TextArea titleBox;
+    public Button prevButton;
+    public Button nxtButton;
+    public TextArea commentBox;
+    public Button comButton;
 
     public void initialize() {
       // scan data file to create all Time objects
@@ -13,4 +23,12 @@ public class Controller {
         TimelinePage p1 = TimelinePage.allTimelinePages.get(0);
     }
 
+    public void pButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void nxtButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void comButtonAction(ActionEvent actionEvent) {
+    }
 }
