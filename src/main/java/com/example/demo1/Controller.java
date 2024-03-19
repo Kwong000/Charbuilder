4aa74b8626786b5442d6ca60dba618aeba6c169a
milesP25 <mprowell25@scienceleadership.org>
2024-03-19 14:08:00 -0400

    yay

--- src/main/java/com/example/demo1/Controller.java
+++ src/main/java/com/example/demo1/Controller.java
@@ -35,9 +35,9 @@ public class Controller {
         TimelinePage p6 = allTimelinePages.get(5);
         // display first TimelineData object in View
 
-        titleBox.setText(allTimelinePages.get(0).getTitle());
-        bodyBox.setText(p1.getTextBody());
         p=1;
+        update();
+
     }
 
     public void nxtButtonAction(ActionEvent actionEvent)throws Exception {
@@ -62,22 +62,32 @@ public class Controller {
         else if (p==2){
             titleBox.setText(allTimelinePages.get(1).getTitle());
             bodyBox.setText(allTimelinePages.get(1).getTextBody());
+            imgBox.setImage(allTimelinePages.get(1).getRepIMG());
+
         }
         else if (p==3){
             titleBox.setText(allTimelinePages.get(2).getTitle());
             bodyBox.setText(allTimelinePages.get(2).getTextBody());
+            imgBox.setImage(allTimelinePages.get(2).getRepIMG());
+
         }
         else if (p==4){
             titleBox.setText(allTimelinePages.get(3).getTitle());
             bodyBox.setText(allTimelinePages.get(3).getTextBody());
+            imgBox.setImage(allTimelinePages.get(3).getRepIMG());
+
         }
         else if (p==5){
             titleBox.setText(allTimelinePages.get(4).getTitle());
             bodyBox.setText(allTimelinePages.get(4).getTextBody());
+            imgBox.setImage(allTimelinePages.get(4).getRepIMG());
+
         }
         else if (p==6){
             titleBox.setText(allTimelinePages.get(5).getTitle());
             bodyBox.setText(allTimelinePages.get(5).getTextBody());
+            imgBox.setImage(allTimelinePages.get(5).getRepIMG());
+
         }
     }
 
